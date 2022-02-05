4c8343f46c248027e51feab99170725d301cf2a4
Anastasia Polivoda <polivoda.anastasiia@gmail.com>
2022-02-05 21:42:40 +0500

    fix: js

--- script.js
+++ script.js
@@ -200,6 +200,7 @@ function formatTime(str) {
 function changeCoverTitleAuthor(index) {
     cover.style.backgroundImage = `url(${track_list[index].image})`;
     document.querySelector('.bgimg').style.backgroundImage = `url(${track_list[index].image})`;
+    
     document.body.style.backgroundImage = `url(${track_list[index].image})`;
     document.body.style.color = track_list[index].textColor;
     document.querySelector('.mask').style.backgroundColor = track_list[index].textColor === '#fff' ? 'rgba(0,0,0,0.5)' : 'rgba(255,255,255,0.5)';
